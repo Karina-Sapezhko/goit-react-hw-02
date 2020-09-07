5d704d73c64ad6265e313096ca9633cccbbf4d4f
Karina-Sapezhko <66127579+Karina-Sapezhko@users.noreply.github.com>
2020-09-07 13:53:11 +0300

    Updates

---
+++ precache-manifest.de642d81c3240e91335c7bb473c72f37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4fb3241d702ac007d0689e8819117b",
+    "url": "/goit-react-hw-02/index.html"
+  },
+  {
+    "revision": "a94e110b411d5a1aae23",
+    "url": "/goit-react-hw-02/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "2f6ec2cd00b65c4546ea",
+    "url": "/goit-react-hw-02/static/css/main.cfac2636.chunk.css"
+  },
+  {
+    "revision": "a94e110b411d5a1aae23",
+    "url": "/goit-react-hw-02/static/js/2.470d29d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02/static/js/2.470d29d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6ec2cd00b65c4546ea",
+    "url": "/goit-react-hw-02/static/js/main.a0fc0894.chunk.js"
+  },
+  {
+    "revision": "cfef1f9ae4146e2f81cd",
+    "url": "/goit-react-hw-02/static/js/runtime-main.9ce766a4.js"
+  }
+]);
